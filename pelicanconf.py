97084ad908695743cb1d43b27f369e3027db0e71
CoeusITE <coeusite@gmail.com>
2017-04-28 14:18:53 +0800

    init config

---
+++ pelicanconf.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'CoeusITE'
+SITENAME = u'CosueITE Closet'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Hong_Kong'
+
+DEFAULT_LANG = u'cn'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'https://getpelican.com/'),
+         ('Python.org', 'https://python.org/'),
+         ('Jinja2', 'https://jinja.pocoo.org/'),
+        )
+
+# Social widget
+SOCIAL = ()
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
